eef451a6a1de53bc7fafc37c2567ff86926c9f2d
Fikunayomi adeleye <fikiman7000@gmail.com>
2023-05-22 15:02:54 +0000

    dao tutorial

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,32 @@
+const { CRYPTODEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+    {
+      value: ethers.utils.parseEther("0.001"),
+    }
+  );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
